36ff0729f20ff11a7f968820647dc5bc32ed60ff
Sheila Adr <siladr2019@gmail.com>
2019-06-13 12:32:13 +0200

    Prueba evaluable - Territorios E1

---
+++ public/js/datos.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDXtU0o3Fss7ekGwFSbo4ZtGsovqQCoplE",
+  authDomain: "territorios-bcc35.firebaseapp.com",
+  databaseURL: "https://territorios-bcc35.firebaseio.com",
+  projectId: "territorios-bcc35",
+  storageBucket: "territorios-bcc35.appspot.com",
+  messagingSenderId: "386589869019",
+  appId: "1:386589869019:web:fe23acbaf279584d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
